eccf89381035c630c1bf5a39b2667a7e93ac10b0
nolannian <hooman.alikhanian@gmail.com>
2025-09-07 14:12:56 -0700

    add first function to helloworld

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,9 @@
 print("hello")
 print("Git from VS Code is easy!")
 
+# writing a function to add two numbers
+def add_numbers(a, b):
+    return a + b
+result = add_numbers(5, 7)
+print("The sum of 5 and 7 is:", result)
+
